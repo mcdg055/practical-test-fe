a88e327a9ec8f1e725d98b478ad2208ec09ea701
mcdg055 <deguzmanmartinc@gmail.com>
2025-05-16 08:54:50 +0800

    setup shadcn ui

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,21 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true,
+    port: 5173,
+  },
+  plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
