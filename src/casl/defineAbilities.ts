b927f94ba8b28b1c6cef739fab75dde09d8c732d
mcdg055 <deguzmanmartinc@gmail.com>
2025-05-27 07:16:31 +0800

    use constant

--- src/casl/defineAbilities.ts
+++ src/casl/defineAbilities.ts
@@ -1,10 +1,11 @@
 import { AbilityBuilder, createMongoAbility } from '@casl/ability'
 import type { AppAbility, Actions, Subjects } from './ability.ts'
+import { ROLE_SUPER_ADMIN } from '@/constants/index.ts'
 
 export function defineAbilitiesFromRolesAndPermissions(roles: string[], permissions: string[]) {
   const { can, rules } = new AbilityBuilder<AppAbility>(createMongoAbility)
 
-  if (roles.includes('Super Admin')) {
+  if (roles.includes(ROLE_SUPER_ADMIN)) {
     can('manage', 'all')
   } else {
     permissions.forEach((permission) => {
